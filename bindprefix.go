8a13edcc731ffab02a60166f050ff36b898d0711
a3d21 <a3d21@outlook.com>
2024-01-18 15:05:12 +0800

    bind prefix

---
+++ bindprefix.go
@@ -0,0 +1,63 @@
+package bind_etcd_cfg
+
+import (
+	"context"
+	clientv3 "go.etcd.io/etcd/client/v3"
+	"strings"
+)
+
+// PrefixSupplier 提供key查val
+type PrefixSupplier[T any] func(key string) (T, bool)
+
+// PrefixListener 值更新时触发
+type PrefixListener[T any] func(eventType EventType, key string, val T)
+
+func BindPrefix[T any](v3cli *clientv3.Client, prefix string, _ T, lis ...PrefixListener[T]) (PrefixSupplier[T], error) {
+	h := &PrefixHolder[T]{}
+
+	ctx := context.Background()
+	get, err := v3cli.Get(ctx, prefix, clientv3.WithPrefix())
+	if err != nil {
+		return nil, err
+	}
+
+	for _, kv := range get.Kvs {
+		k := strings.TrimPrefix(string(kv.Key), prefix)
+		v := string(kv.Value)
+		if k != "" && v != "" {
+			if err2 := h.Refresh(EventTypePut, k, v); err2 != nil {
+				return nil, err2
+			}
+		}
+	}
+
+	// lis
+	for _, li := range lis {
+		h.Range(li)
+	}
+
+	// watch
+	ch := v3cli.Watch(ctx, prefix, clientv3.WithPrefix())
+	go func() {
+		for w := range ch {
+			for _, evt := range w.Events {
+				if k := strings.TrimPrefix(string(evt.Kv.Key), prefix); k != "" {
+					v := string(evt.Kv.Value)
+					if err2 := h.Refresh(evt.Type, k, v); err2 != nil {
+						defaultLogger.Errorf("refresh fail, type: %v key: %v val: %v err: %v", evt.Type, k, v, err2)
+					} else {
+						var t T
+						if evt.Type == EventTypePut {
+							t, _ = h.GetByKey(k)
+						}
+						for _, li := range lis {
+							li(evt.Type, k, t)
+						}
+					}
+				}
+			}
+		}
+	}()
+
+	return h.GetByKey, nil
+}
--- model.go
+++ model.go
@@ -3,12 +3,60 @@ package bind_etcd_cfg
 import (
 	"encoding/json"
 	"errors"
+	"go.etcd.io/etcd/api/v3/mvccpb"
 	"gopkg.in/yaml.v3"
 	"reflect"
 	"strings"
+	"sync"
 
 	"sync/atomic"
 )
+
+type EventType = mvccpb.Event_EventType
+
+const (
+	EventTypePut    EventType = mvccpb.PUT
+	EventTypeDelete EventType = mvccpb.DELETE
+)
+
+type PrefixHolder[T any] struct {
+	data sync.Map
+}
+
+func (h *PrefixHolder[T]) Refresh(eventType EventType, key, raw string) error {
+	if eventType == EventTypeDelete {
+		h.data.Delete(key)
+		return nil
+	}
+	if eventType == EventTypePut {
+		t := new(T)
+		if err := unmarshal(raw, t); err != nil {
+			return err
+		}
+		h.data.Store(key, *t)
+		return nil
+	}
+	return nil
+}
+
+func (h *PrefixHolder[T]) GetByKey(key string) (T, bool) {
+	if v, ok := h.data.Load(key); ok {
+		if vv, ok2 := v.(T); ok2 {
+			return vv, true
+		}
+	}
+	var empty T
+	return empty, false
+}
+
+func (h *PrefixHolder[T]) Range(li PrefixListener[T]) {
+	h.data.Range(func(key, value any) bool {
+		k := key.(string)
+		v := value.(T)
+		li(EventTypePut, k, v)
+		return true
+	})
+}
 
 // Holder is a Dynamic Config Holder with `Refresh`
 type Holder struct {
