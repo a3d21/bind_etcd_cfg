1dbb79a76cce0e32923d923c116b431a9c7e926c
a3d21 <a3d21@outlook.com>
2024-04-10 22:49:58 +0800

    MockPrefix()

--- mock.go
+++ mock.go
@@ -6,3 +6,20 @@ func Mock[T any](v T) Supplier[T] {
 		return v
 	}
 }
+
+func MockPrefix[T any](m map[string]T) PrefixSupplier[T] {
+	return &mockPrefixSupplier[T]{data: m}
+}
+
+type mockPrefixSupplier[T any] struct {
+	data map[string]T
+}
+
+func (m *mockPrefixSupplier[T]) Get(key string) (T, bool) {
+	t, ok := m.data[key]
+	return t, ok
+}
+
+func (m *mockPrefixSupplier[T]) ToMap() map[string]T {
+	return m.data
+}
